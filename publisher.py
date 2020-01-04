563d64ce214dbde3f683bc0be4c19225f9a65db1
eg0rmeister <karpuhin.egor@gmail.com>
2020-01-04 08:01:39 +0000

    branch test

---
+++ publisher.py
@@ -0,0 +1,12 @@
+#!C:\\Users\\karpu\\AppData\\Local\\Programs\\Python\\Python38
+
+
+import paho.mqtt.client as mqtt
+client = mqtt.Client()
+client.connect("localhost")
+while True:
+    i = input("topic(spacebar)message:\n").split()
+    try:
+        client.publish(i[0], i[1])
+    except IndexError:
+        continue
