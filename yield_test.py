b24bdcdb6a883a81a5d014359cd779514312f522
eg0rmeister <karpuhin.egor@gmail.com>
2020-01-05 17:53:19 +0000

    always for u

--- yield_test.py
+++ yield_test.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python3
+
 def foo(num):
-    while True:
-        num += 1
-        yield num
+	while True:
+		num += 1
+		yield num
 val = foo(0)
 while True:
-    print(next(val))
-    a = input()
-    try:
-        a = int(a)
-        val = foo(a)
-    except ValueError:
-        if a == "exit":
-            exit()
+	print(next(val))
+	a = input()
+	try:
+		a = int(a)
+		val = foo(a)
+	except ValueError:
+		if a == "exit":
+			exit()
