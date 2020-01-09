c17afe07c88a43a9dcc09dcb160185c038531d4f
eg0rmeister <karpuhin.egor@gmail.com>
2020-01-09 05:22:33 +0000

    2020-01-09-05:22:33

--- yield_test.py
+++ yield_test.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
 def foo(num):
-	while True:
-		num += 1
-		yield num
-val = foo(0)
+    k = num
+    while True:
+        num += 1
+        print(str(k) + " is now " + str(num))
+        yield num
+val = [foo(0)]
 while True:
-	print(next(val))
-	a = input()
-	try: #if input is a number
-		a = int(a)
-		val = foo(a)
-	except ValueError: #if input is not a number
-		if a == "exit":
-			exit()
+    for i in val:
+        print(next(i))
+    a = input("input:")
+    try:
+        a = int(a)
+        val.append(foo(a))
+    except ValueError:
+        if a == "exit":
+            exit()
