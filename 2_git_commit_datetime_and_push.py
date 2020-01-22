8d4414028bd9c5d3ea99a2330360d2b58c452b31
eg0rmeister <karpuhin.egor@gmail.com>
2020-01-22 20:35:58 +0300

    2020-01-22-20:35:58

--- 2_git_commit_datetime_and_push.py
+++ 2_git_commit_datetime_and_push.py
@@ -5,7 +5,7 @@ import subprocess
 from getpass import getpass
 date = subprocess.check_output(["date", "+%F-%H:%M:%S"]).decode("utf-8")
 print(date)
-#os.system("git add *")
+os.system("git add *")
 os.system("git commit -m " + date)
 branch = subprocess.check_output(["git", "status"]).decode("utf-8").split("\n")[0].split()[2]
 print(branch)
