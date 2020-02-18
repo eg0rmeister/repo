a43f40a9775210a99b88750a8266d07c67b0784e
egor <karpuhin.egor@gmail.com>
2020-02-18 14:01:00 +0000

    2020-02-18-14:01:00

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import serial
-ser = serial.Serial("/dev/ttyACM0", timeout=1000)
+ser = serial.Serial("/dev/ttyACM0", timeout=1)
 while True:
-	b = ser.read()
-	print(b)
-	try:
-		ser.reset_output_buffer()
-		a = '5' + ' ' + str(ord(b) * 255)
-		ser.reset_input_buffer()
-		ser.write(a.encode("UTF-8"))
-	except TypeError:
-		print("something bad happens")
+	if ser.in_waiting > 0:
+		b = ser.readline()
+		print(b)
+		try:
+			a = '5' + ' ' + str(int(b[:-1])*255)
+			ser.reset_input_buffer()
+			ser.reset_output_buffer()
+			ser.write(a.encode("UTF-8"))
+		except ValueError:
+			print("bad")
--- test1.py
+++ test1.py
@@ -1,5 +1,14 @@
 import serial
-ser = serial.Serial("/dev/ttyACM0")
+ser = serial.Serial("/dev/ttyACM0", timeout=1)
 while True:
-	a = ser.readline().decode("UTF-8")
-	print(a)
+ if ser.in_waiting > 0:
+  b = ser.readline()
+  print(b)
+  ser.reset_output_buffer()
+  ser.reset_input_buffer()
+  a = b.split()
+  print(a)
+  for i in range(len(a)):
+   a[i] = int(a[i])
+  print('a =', a)
+  print(b)
